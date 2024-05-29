a659966d0afa0a494490d6a776c60cf299a35069
Бог <@DESKTOP-KSEDEH2>
2024-05-29 20:31:10 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,223 @@
+﻿namespace Уравнения
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea4 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend4 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.textBox9 = new System.Windows.Forms.TextBox();
+            this.textBox10 = new System.Windows.Forms.TextBox();
+            this.textBox11 = new System.Windows.Forms.TextBox();
+            this.textBox12 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.textBox1.Location = new System.Drawing.Point(1034, 154);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(374, 38);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.textBox3.Location = new System.Drawing.Point(1034, 230);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(374, 38);
+            this.textBox3.TabIndex = 5;
+            this.textBox3.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox3_KeyPress);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.button2.Location = new System.Drawing.Point(935, 455);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(473, 111);
+            this.button2.TabIndex = 16;
+            this.button2.Text = "Вывести макс. высоту и расстояние";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.button3.Location = new System.Drawing.Point(935, 305);
+            this.button3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(473, 111);
+            this.button3.TabIndex = 19;
+            this.button3.Text = "Построить график";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // chart1
+            // 
+            chartArea4.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea4);
+            legend4.Enabled = false;
+            legend4.ItemColumnSpacing = 200;
+            legend4.Name = "Legend1";
+            this.chart1.Legends.Add(legend4);
+            this.chart1.Location = new System.Drawing.Point(13, 13);
+            this.chart1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.chart1.Name = "chart1";
+            series4.ChartArea = "ChartArea1";
+            series4.Legend = "Legend1";
+            series4.Name = "Series1";
+            this.chart1.Series.Add(series4);
+            this.chart1.Size = new System.Drawing.Size(855, 831);
+            this.chart1.TabIndex = 20;
+            this.chart1.Text = "chart1";
+            // 
+            // textBox9
+            // 
+            this.textBox9.Location = new System.Drawing.Point(876, 13);
+            this.textBox9.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox9.Name = "textBox9";
+            this.textBox9.Size = new System.Drawing.Size(56, 31);
+            this.textBox9.TabIndex = 26;
+            // 
+            // textBox10
+            // 
+            this.textBox10.Location = new System.Drawing.Point(876, 813);
+            this.textBox10.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox10.Name = "textBox10";
+            this.textBox10.Size = new System.Drawing.Size(57, 31);
+            this.textBox10.TabIndex = 27;
+            // 
+            // textBox11
+            // 
+            this.textBox11.Location = new System.Drawing.Point(795, 852);
+            this.textBox11.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox11.Name = "textBox11";
+            this.textBox11.Size = new System.Drawing.Size(73, 31);
+            this.textBox11.TabIndex = 28;
+            // 
+            // textBox12
+            // 
+            this.textBox12.Location = new System.Drawing.Point(13, 852);
+            this.textBox12.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox12.Name = "textBox12";
+            this.textBox12.Size = new System.Drawing.Size(60, 31);
+            this.textBox12.TabIndex = 29;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial Narrow", 10.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(929, 158);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(104, 31);
+            this.label1.TabIndex = 30;
+            this.label1.Text = "1)   Угол:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial Narrow", 10.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(929, 234);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(97, 31);
+            this.label3.TabIndex = 32;
+            this.label3.Text = "2)   Шаг:";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(935, 599);
+            this.textBox4.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(473, 84);
+            this.textBox4.TabIndex = 33;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoValidate = System.Windows.Forms.AutoValidate.EnableAllowFocusChange;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(1459, 914);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox12);
+            this.Controls.Add(this.textBox11);
+            this.Controls.Add(this.textBox10);
+            this.Controls.Add(this.textBox9);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Задача 6";
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.TextBox textBox9;
+        private System.Windows.Forms.TextBox textBox10;
+        private System.Windows.Forms.TextBox textBox11;
+        private System.Windows.Forms.TextBox textBox12;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,242 @@
+﻿
+using System;
+using System.Windows.Forms;
+using System.Numerics;
+using System.Windows.Forms.DataVisualization.Charting;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using System.Linq;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using System.Drawing.Design;
+
+namespace Уравнения
+{
+    public partial class Form1 : Form
+    {
+        public double xt;
+        public Form1()
+        {
+            InitializeComponent();
+            textBox12.Text = "0";
+            textBox11.Text = "10";
+            textBox10.Text = "0";
+            textBox9.Text = "10";
+        }
+
+        public class Point
+        {
+            public double X { get; set; }
+            public double Y { get; set; }
+
+            public Point(double x, double y)
+            {
+                X = x;
+                Y = y;
+            }
+
+            public override string ToString()
+            {
+                return $"({X}, {Y})";
+            }
+        }
+        private List<Point> pointsList = new List<Point>();
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox4.Clear();
+            textBox4.Text += xt;
+
+        }
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == 45 && textBox1.SelectionStart == 0) {; }
+            else
+            {
+                if (e.KeyChar == '.') e.KeyChar = ',';
+                if (e.KeyChar != 13 && e.KeyChar != 44 && e.KeyChar != 8 && (e.KeyChar < 48 || e.KeyChar > 57))
+                {
+                    e.Handled = true;
+                }
+            }
+        }
+
+        private void textBox3_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == 45 && textBox3.SelectionStart == 0) {; }
+            else
+            {
+                if (e.KeyChar == '.') e.KeyChar = ',';
+                if (e.KeyChar != 13 && e.KeyChar != 44 && e.KeyChar != 8 && (e.KeyChar < 48 || e.KeyChar > 57))
+                {
+                    e.Handled = true;
+                }
+            }
+        }
+
+
+
+        private void textBox9_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == 45 && textBox9.SelectionStart == 0) {; }
+            else
+            {
+                if (e.KeyChar == '.') e.KeyChar = ',';
+                if (e.KeyChar != 13 && e.KeyChar != 44 && e.KeyChar != 8 && (e.KeyChar < 48 || e.KeyChar > 57))
+                {
+                    e.Handled = true;
+                }
+            }
+        }
+        private void textBox10_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == 45 && textBox10.SelectionStart == 0) {; }
+            else
+            {
+                if (e.KeyChar == '.') e.KeyChar = ',';
+                if (e.KeyChar != 13 && e.KeyChar != 44 && e.KeyChar != 8 && (e.KeyChar < 48 || e.KeyChar > 57))
+                {
+                    e.Handled = true;
+                }
+            }
+        }
+        private void textBox11_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == 45 && textBox11.SelectionStart == 0) {; }
+            else
+            {
+                if (e.KeyChar == '.') e.KeyChar = ',';
+                if (e.KeyChar != 13 && e.KeyChar != 44 && e.KeyChar != 8 && (e.KeyChar < 48 || e.KeyChar > 57))
+                {
+                    e.Handled = true;
+                }
+            }
+        }
+        private void textBox12_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == 45 && textBox12.SelectionStart == 0) {; }
+            else
+            {
+                if (e.KeyChar == '.') e.KeyChar = ',';
+                if (e.KeyChar != 13 && e.KeyChar != 44 && e.KeyChar != 8 && (e.KeyChar < 48 || e.KeyChar > 57))
+                {
+                    e.Handled = true;
+                }
+            }
+        }
+
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+
+            textBox9.Visible = true;
+            textBox10.Visible = true;
+            textBox11.Visible = true;
+            textBox12.Visible = true;
+            chart1.Visible = true;
+            chart1.Series.Clear(); chart1.ChartAreas.Clear();
+            chart1.ChartAreas.Add(new ChartArea());
+            double xmin = Convert.ToDouble(textBox12.Text);
+            double xmax = Convert.ToDouble(textBox11.Text);
+            double ymin = Convert.ToDouble(textBox10.Text);
+            double ymax = Convert.ToDouble(textBox9.Text);
+            chart1.ChartAreas[0].AxisX.MajorGrid.Enabled = false;
+            chart1.ChartAreas[0].AxisY.MajorGrid.Enabled = false;
+            chart1.ChartAreas[0].AxisX.Crossing = 0;
+            chart1.ChartAreas[0].AxisY.Crossing = 0;
+            chart1.MouseWheel += Chart1_MouseWheel;
+            chart1.ChartAreas[0].AxisX.ScaleView.Zoomable = true;
+            chart1.ChartAreas[0].AxisY.ScaleView.Zoomable = true;
+            chart1.ChartAreas[0].AxisX.Minimum = xmin;
+            chart1.ChartAreas[0].AxisX.Maximum = xmax; ;
+            chart1.ChartAreas[0].AxisY.Minimum = ymin; ;
+            chart1.ChartAreas[0].AxisY.Maximum = ymax; ;
+            chart1.ChartAreas[0].AxisX.Interval = 1;
+            chart1.ChartAreas[0].AxisX.LabelStyle.Interval = 5;
+            chart1.ChartAreas[0].AxisY.Interval = 1;
+            chart1.ChartAreas[0].AxisY.LabelStyle.Interval = 5;
+            chart1.ChartAreas[0].AxisX.MajorGrid.LineColor = System.Drawing.Color.Gray;
+            chart1.ChartAreas[0].AxisY.MajorGrid.LineColor = System.Drawing.Color.Gray;
+            chart1.ChartAreas[0].AxisY.ScaleView.ZoomReset();
+
+            Series series = new Series();
+            Series series1 = new Series();
+            Series series2 = new Series();
+            series.ChartType = SeriesChartType.Spline;
+            series.Points.Clear();
+            series1.ChartType = SeriesChartType.Spline;
+            series1.Points.Clear();
+            series.BorderWidth = 2;
+            series2.ChartType = SeriesChartType.Spline;
+            series2.Points.Clear();
+            double step = Convert.ToDouble(textBox3.Text);
+            double angle = Convert.ToDouble(textBox1.Text);
+            double Vx = 1, Vy = 1, x = 0, y = 0, vx1 = 0, vy1 = 0, c = 0.0075, g = 9.81,
+                m = 120, mk = 15, a = 10, vg = 1800, v0 = 20, t = 0, mt = 0, T = (m - mk) / a;
+
+            angle = angle * Math.PI / 180;
+            Vx = v0 * Math.Cos(angle);
+            Vy = v0 * Math.Sin(angle);
+            while (y >= 0)
+            {
+                series.Points.AddXY(x / 1000, y / 1000);
+                vx1 = Vx;
+                vy1 = Vy;
+                t += step;
+                if (t <= T)
+                {
+                    mt = 120 - t * a;
+                    Vx += (a * vg * vx1 / Math.Sqrt(vx1 * vx1 + vy1 * vy1) - c * Math.Sqrt(vx1 * vx1 + vy1 * vy1) * vx1) / mt * step;
+                    Vy += (-g + (a * vg * vy1 / Math.Sqrt(vx1 * vx1 + vy1 * vy1) - c * Math.Sqrt(vx1 * vx1 + vy1 * vy1) * vy1) / mt) * step;
+        
+                }
+                else
+                {
+                    if (Math.Round(t, 1) == T+step) xt = x / 1000;
+                    Vx += (-c * Math.Sqrt(vx1 * vx1 + vy1 * vy1) * vx1) / mk * step;
+                    Vy += (-g - (c * Math.Sqrt(vx1 * vx1 + vy1 * vy1) * vy1) / mk) * step;
+                }
+                x += Vx * step;
+                y += Vy * step;
+
+            }
+
+            chart1.Series.Add(series);
+        }
+        private void Chart1_MouseWheel(object sender, MouseEventArgs e)
+        {
+            int delta = e.Delta;
+
+            double currentMinX = chart1.ChartAreas[0].AxisX.Minimum;
+            double currentMaxX = chart1.ChartAreas[0].AxisX.Maximum;
+            double currentMinY = chart1.ChartAreas[0].AxisY.Minimum;
+            double currentMaxY = chart1.ChartAreas[0].AxisY.Maximum;
+
+            double newMinX = currentMinX - delta * 0.01;
+            double newMaxX = currentMaxX + delta * 0.01;
+            double newMinY = currentMinY - delta * 0.01;
+            double newMaxY = currentMaxY + delta * 0.01;
+
+            if (newMinX < newMaxX)
+            {
+                chart1.ChartAreas[0].AxisX.Minimum = newMinX;
+                chart1.ChartAreas[0].AxisX.Maximum = newMaxX;
+            }
+            if (newMinY < newMaxY)
+            {
+                chart1.ChartAreas[0].AxisY.Minimum = newMinY;
+                chart1.ChartAreas[0].AxisY.Maximum = newMaxY;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
